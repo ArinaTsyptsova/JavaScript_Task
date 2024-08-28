66cdbabaacee3f6658bc5f0031a7092fcaa3ca63
Arina <arinacypcova@gmail.com>
2024-08-28 18:34:44 +0700

    HW_1

---
+++ HW_1/script.js
@@ -0,0 +1,10 @@
+//Task1
+let tempCelsius = 10;
+let tempFahrenheit = (9 / 5) * tempCelsius + 32;
+console.log(tempCelsius);
+console.log(tempFahrenheit);
+
+//Task2
+const name = "Арина";
+const admin = name;
+console.log(admin);
